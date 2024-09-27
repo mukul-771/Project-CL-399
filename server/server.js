83dc24c8fd9e25f766cc23c0a5abe0a86882e548
mukul meena <mukul7711@mukuls-MacBook-Air-2800.local>
2024-09-27 15:14:20 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 // Middleware to parse JSON
 app.use(express.json());
